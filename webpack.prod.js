5db2fb99e118a17f620f21e89f32ff1be2f465a3
Hans Blum <hans.blum@capgemini.com>
2019-10-07 20:54:37 +0200

    Favoured the working clean-webpack-plugin over the non working webpack-clean-plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,11 @@
 const merge = require('webpack-merge');
 const webpackCommonConf = require('./webpack.common');
-const WebpackCleanPlugin = require('webpack-clean-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = merge(webpackCommonConf, {
   mode: 'production',
   plugins: [
-    new WebpackCleanPlugin({
-      on: 'emit',
-      path: 'dist'
-    })
+    new CleanWebpackPlugin()
   ]
 
 });
